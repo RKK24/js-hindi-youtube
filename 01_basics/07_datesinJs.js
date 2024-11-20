54f03250bac3b9d8b4b700beedc696d0633c4613
RKK24 <rutvi.k0302@gmail.com>
2024-11-20 14:07:22 +0000

    Dates in JS

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,27 +1,27 @@
 // Dates
 let myDate = new Date()
-// console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
 
 let myCreatedDate = new Date(2024, 10, 20);
-// console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toDateString());
 
-// let myCreatedDate2 = new Date(2024, 10, 20, 18, 39);
-// console.log(myCreatedDate2.toLocaleString());
+let myCreatedDate2 = new Date(2024, 10, 20, 18, 39);
+console.log(myCreatedDate2.toLocaleString());
 
-// let myCreatedDate3 = new Date("2024-11-20");
-// console.log(myCreatedDate3.toLocaleString());
+let myCreatedDate3 = new Date("2024-11-20");
+console.log(myCreatedDate3.toLocaleString());
 
-// let myCreatedDate4 = new Date("11-20-2024");
-// console.log(myCreatedDate4.toLocaleString());
+let myCreatedDate4 = new Date("11-20-2024");
+console.log(myCreatedDate4.toLocaleString());
 
-// let myTimeStamp = Date.now();
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date();
 console.log(newDate);
