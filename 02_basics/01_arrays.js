7613c221a316a300587cf14248758629b3c88cea
RKK24 <rutvi.k0302@gmail.com>
2024-11-21 07:35:03 +0000

    Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -22,7 +22,7 @@ console.log(myArr);
 console.log(newArr);
 console.log( typeof newArr);
 
-// slice, splice
+slice, splice
 
 console.log("A ", myArr);
 const myn1 = myArr.slice(1, 3);
@@ -33,3 +33,31 @@ console.log("B ", myArr);
 const myn2 = myArr.splice(1, 3);
 console.log(myn2);
 console.log("C ",myArr);
+
+const marvel_heros = ["thor", "ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Jhon"));
+console.log(Array.from("Jhon"));
+console.log(Array.from({name: "Jhon"})); // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+
