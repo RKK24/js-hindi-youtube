f4c764ee34cf9540cf67462f34f2c27d3aca0e51
RKK24 <rutvi.k0302@gmail.com>
2024-11-22 13:58:54 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,13 +4,52 @@ if (true) {
     let a = 10;
     const b = 20;
     // var c = 30; 
-    console.log("INNER: ", a);   
+    // console.log("INNER: ", a);   
 }
-
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
 // }
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Jhon";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+if (true) {
+    const username = "Jhon";
+    if(username === "Jhon") {
+        const website = "youtube";
+        console.log(username+website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// **************** interesting concept ****************
+
+console.log(addone(5));
+function addone(num){
+    return num+1;
+}
+
+
+console.log(addtwo(5));
+const addtwo = function(num){ // expression
+    return num+2;
+}
+
+
+
